18717ba313f2e07ec658d20a9bc6b6537a14d3b0
unknown <mh.naderi1386@gmail.com>
2025-05-15 15:59:51 +0330

    add the input search

--- app/_components/HeaderInputSearch.tsx
+++ app/_components/HeaderInputSearch.tsx
@@ -4,7 +4,24 @@ import { useState } from "react";
 
 const HeaderInputSearch = () => {
   const [inputSearch, setInputSearch] = useState("");
-  return <form>HeaderInputSearch</form>;
+  const handleSubmit = () => {};
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input
+          type="text"
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+          className="border-none outline-none"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default HeaderInputSearch;
