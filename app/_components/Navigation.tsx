e8bd3fabbffe462ded2487efcf9811bfab250fb7
unknown <mh.naderi1386@gmail.com>
2025-05-15 14:02:58 +0330

    add some new items for navigation items

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,8 +1,13 @@
+const navigationItems = [
+  { text: "BBC News", href: "/news?sources=bbc-news" },
+  {
+    text: "United States",
+    href: "/news?country=us",
+  },
+];
 
 const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
-}
+  return <div>Navigation</div>;
+};
 
-export default Navigation
+export default Navigation;
