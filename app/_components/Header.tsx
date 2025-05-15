1ea7bfc353630c654293a6a525c44c10b0d5221a
unknown <mh.naderi1386@gmail.com>
2025-05-15 14:34:02 +0330

    add a new headercontent component to make the code cleaner

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,11 @@
-import Navigation from "./Navigation"
+import HeaderContent from "./HeaderContent";
 
 const Header = () => {
   return (
     <header className="bg-custome-dark px-18 py-6">
-      <Navigation />
+      <HeaderContent />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ app/_components/HeaderContent.tsx
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+
+const HeaderContent = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <Navigation />
+    </div>
+  );
+};
+
+export default HeaderContent;
