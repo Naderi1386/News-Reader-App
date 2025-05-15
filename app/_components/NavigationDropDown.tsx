e2bb2c045d06e2b587f61cd9c9153937bbb873ec
unknown <mh.naderi1386@gmail.com>
2025-05-15 14:24:39 +0330

    add dropdown items

--- app/_components/NavigationDropDown.tsx
+++ app/_components/NavigationDropDown.tsx
@@ -1,3 +1,12 @@
+const DropdownItems = [
+  { text: "business", href: "/news?category=business" },
+  { text: "entertainment", href: "/news?category=entertainment" },
+  { text: "general", href: "/news?category=general" },
+  { text: "health", href: "/news?category=health" },
+  { text: "science", href: "/news?category=science" },
+  { text: "sports", href: "/news?category=sports" },
+  { text: "technology", href: "/news?category=technology" },
+];
 
 const NavigationDropDown = () => {
   return (
@@ -11,7 +20,7 @@ const NavigationDropDown = () => {
       </span>
       <ul
         tabIndex={0}
-        className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm"
       >
         <li>
           <a>Item 1</a>
@@ -22,6 +31,6 @@ const NavigationDropDown = () => {
       </ul>
     </div>
   );
-}
+};
 
-export default NavigationDropDown
+export default NavigationDropDown;
