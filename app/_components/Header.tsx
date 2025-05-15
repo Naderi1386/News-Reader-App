bc41e07836107ad0322c5d0555d19307a6e7ad77
unknown <mh.naderi1386@gmail.com>
2025-05-15 14:11:31 +0330

    change styles in navigation items

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,9 @@
+import Navigation from "./Navigation"
 
 const Header = () => {
   return (
-    <header className="bg-custome-dark px-18 py-5">
-      
+    <header className="bg-custome-dark px-18 py-6">
+      <Navigation />
     </header>
   )
 }
--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const navigationItems = [
   { text: "BBC News", href: "/news?sources=bbc-news" },
   {
@@ -7,7 +9,21 @@ const navigationItems = [
 ];
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="text-white">
+      <nav>
+        <ul className="flex items-center gap-4">
+          {navigationItems.map((navItem) => (
+            <li key={navItem.href}>
+              <Link href={navItem.href} className="block cursor-pointer font-semibold transition-all duration-500 hover:underline-offset-8 hover:text-custome-green hover:underline">
+                {navItem.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
