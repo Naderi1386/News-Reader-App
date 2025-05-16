b104b6485f8b8f35f93982a5c5b13c41e8d891b4
unknown <mh.naderi1386@gmail.com>
2025-05-16 20:26:01 +0330

    working on news poster

--- app/_components/NewsPoster.tsx
+++ app/_components/NewsPoster.tsx
@@ -4,8 +4,8 @@ interface NewsPosterPropsType{
     img:string
 }
 const NewsPoster = ({img}: NewsPosterPropsType) => {
-  return <div className="relative w-[175px] h-[95px]">
-    <Image fill quality={90} src={img} className="object-cover" alt="NEWS-POSTER" />
+  return <div className="relative w-[175px] h-[95px] border border-solid border-black/10">
+    <Image  fill quality={90} src={img} className="object-cover" alt="NEWS-POSTER" />
   </div>;
 };
 
