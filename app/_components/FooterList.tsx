b13b8bd1534f4d55a9489c6908e34a6091bbc5fc
unknown <mh.naderi1386@gmail.com>
2025-05-16 18:17:31 +0330

    fix a bug and change ui in footer list

--- app/_components/FooterList.tsx
+++ app/_components/FooterList.tsx
@@ -6,9 +6,12 @@ interface FooterListPropsType {
 }
 const FooterList = ({ items }: FooterListPropsType) => {
   return (
-    <ul className="flex flex-col gap-4 text-white">
+    <ul className="flex flex-col gap-2  font-semibold">
       {items.map((item) => (
-        <li key={item.href}>
+        <li
+          key={item.href}
+          className="text-white transition-all duration-200 hover:text-custome-green"
+        >
           <Link href={item.href}>{item.text}</Link>
         </li>
       ))}
--- app/_components/FooterLists.tsx
+++ app/_components/FooterLists.tsx
@@ -1,7 +1,19 @@
+import FooterList from "./FooterList";
+
 const FooterLists = () => {
   return (
-    <div className="pr-12">
-      <FooterLists />
+    <div className="pr-34">
+      <FooterList
+        items={[
+          { text: "business", href: "/news?category=business" },
+          { text: "entertainment", href: "/news?category=entertainment" },
+          { text: "general", href: "/news?category=general" },
+          { text: "health", href: "/news?category=health" },
+          { text: "science", href: "/news?category=science" },
+          { text: "sports", href: "/news?category=sports" },
+          { text: "technology", href: "/news?category=technology" },
+        ]}
+      />
     </div>
   );
 };
