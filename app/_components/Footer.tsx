9d97f310b9159338f5108ec8414404abcb684384
unknown <mh.naderi1386@gmail.com>
2025-05-16 18:25:44 +0330

    make footer responsive just with flex-wrap

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 const Footer = () => {
   return (
     <footer className="px-4 md:px-18 py-10 bg-custome-dark">
-      <div className="flex items-start justify-between">
+      <div className="flex gap-16 flex-wrap items-start justify-between">
         <Logo type="footer" />
         <FooterLists />
       </div>
