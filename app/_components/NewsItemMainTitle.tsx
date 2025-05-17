af4c0e86f09e20180a6fca2ca48af7372e610a20
unknown <mh.naderi1386@gmail.com>
2025-05-17 23:27:11 +0330

    change ui and fix a problem

--- app/_components/NewsItemMainTitle.tsx
+++ app/_components/NewsItemMainTitle.tsx
@@ -4,7 +4,7 @@ interface NewsItemMainTitlePropsType {
   title: string;
 }
 const NewsItemMainTitle = ({ title }: NewsItemMainTitlePropsType) => {
-  return <Link href={"/"} className="text-black font-bold text-lg mt-2">{title}</Link>;
+  return <Link href={"/"} className="text-black font-bold text-lg hover:underline block">{title}</Link>;
 };
 
 export default NewsItemMainTitle;
