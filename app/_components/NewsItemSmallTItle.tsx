6041a0e3eccb1887887565941b32d1afd6f3fbd0
unknown <mh.naderi1386@gmail.com>
2025-05-17 23:14:56 +0330

    working on newsitem component

--- app/_components/NewsItemSmallTItle.tsx
+++ app/_components/NewsItemSmallTItle.tsx
@@ -1,10 +1,19 @@
-"use client"
-const NewsItemSmallTItle = () => {
-  return (
-    <span >
-    
-    </span>
-  )
-}
+"use client";
 
-export default NewsItemSmallTItle
+import { useSearchParams } from "next/navigation";
+
+const NewsItemSmallTItle = () => {
+  const searchParams = useSearchParams();
+  const title =
+    searchParams.get("source") ||
+    searchParams.get("category") ||
+    searchParams.get("country") ||
+    "";
+  return (
+    <span className="text-custome-green text-sm font-light transition-all duration-150 pt-2 hover:border hover:border-stone-300 hover:border-solid">
+      {title}
+    </span>
+  );
+};
+
+export default NewsItemSmallTItle;
