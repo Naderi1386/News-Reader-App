640ef27455c21380aa9b7417305910ebbf6272b3
unknown <mh.naderi1386@gmail.com>
2025-05-18 15:40:06 +0330

    add a default poster for news poster when it has an error

--- app/_components/NewsPoster.tsx
+++ app/_components/NewsPoster.tsx
@@ -9,7 +9,14 @@ const NewsPoster = ({ img }: NewsPosterPropsType) => {
   const proxiedSrc = `/api/image-proxy?url=${encodeURIComponent(img)}`;
   const [src, setSrc] = useState(proxiedSrc);
   return (
-    <Image width={150} height={100} quality={90} src={src} alt="NEWS-POSTER" />
+    <Image
+      width={150}
+      height={100}
+      quality={90}
+      src={src}
+      alt="NEWS-POSTER"
+      onError={() => setSrc("/images/empty-news.jpg")}
+    />
   );
 };
 
