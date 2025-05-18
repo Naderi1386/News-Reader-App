9ee3e1dce36fca838645d17cb947e0391bb1a65d
unknown <mh.naderi1386@gmail.com>
2025-05-18 13:37:10 +0330

    change ui in items published date

--- app/_components/NewsItemPublishedDate.tsx
+++ app/_components/NewsItemPublishedDate.tsx
@@ -1,8 +1,11 @@
+import { timeAgo } from "../_lib/helper";
+
 interface NewsItemPublishedDatePropsType {
   date: string;
 }
 const NewsItemPublishedDate = ({ date }: NewsItemPublishedDatePropsType) => {
-  return <span>{date}</span>;
+  const publishedDate=timeAgo(date)
+  return <span className="text-stone-500 text-xs capitalize block">{publishedDate}</span>;
 };
 
 export default NewsItemPublishedDate;
