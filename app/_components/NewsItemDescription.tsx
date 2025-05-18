0af8f00e6b202aab08631be912949b8bb47c9df7
unknown <mh.naderi1386@gmail.com>
2025-05-18 15:14:51 +0330

    working on responsive of news item

--- app/_components/NewsItemDescription.tsx
+++ app/_components/NewsItemDescription.tsx
@@ -2,7 +2,7 @@ interface NewsItemDescriptionPropsType {
   description: string;
 }
 const NewsItemDescription = ({ description }: NewsItemDescriptionPropsType) => {
-  return <p className="font-semibold text-stone-500 text-[14.5px] w-[65%] text-justify">{description}</p>;
+  return <p className="font-semibold text-stone-500 text-[12px] md:text-[14.5px] w-[65%] text-justify">{description}</p>;
 };
 
 export default NewsItemDescription;
