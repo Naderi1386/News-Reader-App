b5c57f9c8434224ab4b0a746c4b2c50623891b06
unknown <mh.naderi1386@gmail.com>
2025-05-18 16:06:20 +0330

    add a state for headerlogowrraper

--- app/_components/HeaderLogoWrraper.tsx
+++ app/_components/HeaderLogoWrraper.tsx
@@ -1,6 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 
 const HeaderLogoWrraper = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  if (!show) return null;
   return (
     <div className="hidden md:block">
       <Logo type="header" />
