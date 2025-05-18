5329947ac0b2b3696a947de9e81fb83ea13450ab
unknown <mh.naderi1386@gmail.com>
2025-05-18 15:38:27 +0330

    create a state for news item poster

--- app/_components/NewsPoster.tsx
+++ app/_components/NewsPoster.tsx
@@ -1,18 +1,15 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 interface NewsPosterPropsType {
   img: string;
 }
 const NewsPoster = ({ img }: NewsPosterPropsType) => {
   const proxiedSrc = `/api/image-proxy?url=${encodeURIComponent(img)}`;
+  const [src, setSrc] = useState(proxiedSrc);
   return (
-    <Image
-      width={150}
-      height={100}
-      quality={90}
-      src={proxiedSrc}
-      alt="NEWS-POSTER"
-    />
+    <Image width={150} height={100} quality={90} src={src} alt="NEWS-POSTER" />
   );
 };
 
