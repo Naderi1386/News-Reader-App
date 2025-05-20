888814e70303869108b2ac7ec5c3fccf8642f334
unknown <mh.naderi1386@gmail.com>
2025-05-20 13:53:26 +0330

    change auth file codes and add client ID and Secret and fixed the problem

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
+
+import { login } from "../_lib/actions";
+
 const page = () => {
   return (
     <div className="bg-custome-green">
       <div className="flex flex-col gap-8 items-center justify-center py-20">
-        <h1 className="capitalize text-white font-bold text-4xl italic">log in with your google account</h1>
-        <button className="btn btn-lg bg-white text-black border-[#e5e5e5]">
+        <h1 className="capitalize text-white font-bold text-4xl italic">
+          log in with your google account
+        </h1>
+        <button
+          onClick={() => login()}
+          className="btn btn-lg bg-white text-black border-[#e5e5e5]"
+        >
           <svg
             aria-label="Google logo"
             width="20"
