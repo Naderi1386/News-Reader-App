14898c65af8b37b90374b328e765e68a0d7e19a5
unknown <mh.naderi1386@gmail.com>
2025-05-20 16:01:52 +0330

    login btn has created

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
-"use client";
+import LoginBtn from "../_components/LoginBtn";
 
-import { login } from "../_lib/actions";
 
 const page = () => {
   return (
@@ -9,39 +8,7 @@ const page = () => {
         <h1 className="capitalize text-white font-bold text-4xl italic">
           log in with your google account
         </h1>
-        <button
-          onClick={() => login()}
-          className="btn btn-lg bg-white text-black border-[#e5e5e5]"
-        >
-          <svg
-            aria-label="Google logo"
-            width="20"
-            height="20"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <g>
-              <path d="m0 0H512V512H0" fill="#fff"></path>
-              <path
-                fill="#34a853"
-                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-              ></path>
-              <path
-                fill="#4285f4"
-                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-              ></path>
-              <path
-                fill="#fbbc02"
-                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-              ></path>
-              <path
-                fill="#ea4335"
-                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-              ></path>
-            </g>
-          </svg>
-          Login with Google
-        </button>
+        <LoginBtn />
       </div>
     </div>
   );
