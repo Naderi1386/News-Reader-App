a0b3cc7e84e9563fb93dd7514dd6bf6c1baffa7d
unknown <mh.naderi1386@gmail.com>
2025-05-20 13:35:15 +0330

    add a link for loging in

--- app/_components/HeaderProfile.tsx
+++ app/_components/HeaderProfile.tsx
@@ -1,7 +1,14 @@
 import { auth } from "@/auth";
+import Link from "next/link";
 
 const HeaderProfile = async () => {
   const session = await auth();
+  if (!session)
+    return (
+      <Link href={"/login"} className="btn btn-dash btn-success">
+        Log In
+      </Link>
+    );
 
   return <div>HeaderProfile</div>;
 };
