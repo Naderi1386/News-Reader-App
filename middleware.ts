f159a926358e12d2ab25cfee13e6d32c2221bf0f
unknown <mh.naderi1386@gmail.com>
2025-05-20 15:57:00 +0330

    working on middleware p1

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,8 @@
+import { NextRequest } from "next/server";
+import { auth } from "./auth";
+
 const protectedRoutes = ["/news", "/search"];
+
+export const middleware = async (request: NextRequest) => {
+  const session = await auth();
+};
