203a5a55bc08f617ce29513bdc363e6c23c61c09
unknown <mh.naderi1386@gmail.com>
2025-05-20 15:59:05 +0330

    working on middleware p2

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 
 const protectedRoutes = ["/news", "/search"];
 
 export const middleware = async (request: NextRequest) => {
   const session = await auth();
+  const { pathname } = request.nextUrl;
+  const isProtected = protectedRoutes.includes(pathname);
+  if (isProtected && !session)
+    return NextResponse.redirect(new URL("/login", request.url));
 };
