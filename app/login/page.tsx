773396ef46b729db1c0d7c00c6d317644032bf60
unknown <mh.naderi1386@gmail.com>
2025-05-22 14:24:44 +0330

    add some responsive styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,8 @@ import LoginBtn from "../_components/LoginBtn";
 const page = () => {
   return (
     <div className="bg-custome-green">
-      <div className="flex flex-col gap-8 items-center justify-center py-20">
-        <h1 className="capitalize text-white font-bold text-4xl italic">
+      <div className="flex flex-col gap-8 items-center justify-center py-20 px-4 sm:px-0">
+        <h1 className="capitalize text-white font-bold text-2xl sm:text-4xl italic text-center sm:text-left">
           log in with your google account
         </h1>
         <LoginBtn />
