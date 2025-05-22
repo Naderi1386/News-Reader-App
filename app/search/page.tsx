306231ec084a7fec73692f82bf43d5b2d6cedda0
unknown <mh.naderi1386@gmail.com>
2025-05-22 17:15:18 +0330

    add a title with metadata for search route

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,6 +6,13 @@ interface PagePropsType {
   searchParams: Promise<SearchParamsType>;
 }
 
+export async function generateMetadata(props: PagePropsType) {
+  const q = (await props.searchParams).q || "search";
+  return {
+    title: `Search for ${q.replaceAll("-", " ")}`,
+  };
+}
+
 const page = async (props: PagePropsType) => {
   const query = (await props.searchParams).q || "search";
   return <div>page</div>;
