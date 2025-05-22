a41a01d003af1151f5340727e0d3dd3df3700532
unknown <mh.naderi1386@gmail.com>
2025-05-22 14:28:09 +0330

    add pathname to show login link when we are not in login route

--- app/_components/LoginLink.tsx
+++ app/_components/LoginLink.tsx
@@ -1,13 +1,16 @@
-"use client"
+"use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const LoginLink = () => {
+  const pathname = usePathname();
+  if (pathname === "/login") return null;
   return (
     <Link href={"/login"} className="btn btn-dash btn-success">
       Log In
     </Link>
   );
-}
+};
 
-export default LoginLink
+export default LoginLink;
