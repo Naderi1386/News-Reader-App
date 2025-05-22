89f061f9401a8d9925dbbd3a3f4a6bbc49e4f2da
unknown <mh.naderi1386@gmail.com>
2025-05-22 18:06:55 +0330

    add getSearchedNews in newslist component

--- app/_components/NewsList.tsx
+++ app/_components/NewsList.tsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 import {
   getBBCNews,
   getNewsByCategory,
+  getSearchedNews,
   getUSNews,
   NewsType,
 } from "../_lib/services";
@@ -11,17 +12,19 @@ interface NewsListPropsType {
   category?: string | undefined;
   sources?: string | undefined;
   country?: string | undefined;
-  q?:string | undefined
+  q?: string | undefined;
 }
 const NewsList = memo(async function NewsList({
   category,
   country,
   sources,
+  q,
 }: NewsListPropsType) {
   let news: NewsType[] = [];
   if (sources) news = (await getBBCNews()).articles;
   if (country) news = (await getUSNews()).articles;
   if (category) news = (await getNewsByCategory(category)).articles;
+  if (q) news = (await getSearchedNews(q)).articles;
   return (
     <ul className="divide-y divide-stone-300">
       {news.map((details, index) => (
