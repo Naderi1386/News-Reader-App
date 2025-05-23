054b6fe6b1e9160791e014bfcdaaa5db7eebbe19
unknown <mh.naderi1386@gmail.com>
2025-05-23 15:58:54 +0330

    show number of page in our pagination

--- app/_components/Pagination.tsx
+++ app/_components/Pagination.tsx
@@ -1,11 +1,22 @@
 "use client";
+
+import { useSearchParams } from "next/navigation";
+
 const Pagination = () => {
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page")?.toString();
   return (
     <div className="flex items-center justify-center mt-6 ">
       <div className="join ">
-        <button className="join-item btn shadow-none bg-custome-green border-white">«</button>
-        <button className="join-item btn shadow-none bg-custome-green border-white">Page 22</button>
-        <button className="join-item btn shadow-none bg-custome-green border-white">»</button>
+        <button className="join-item btn shadow-none bg-custome-green border-white">
+          «
+        </button>
+        <button className="join-item btn shadow-none bg-custome-green border-white">
+          Page {page ? page : "1"}
+        </button>
+        <button className="join-item btn shadow-none bg-custome-green border-white">
+          »
+        </button>
       </div>
     </div>
   );
