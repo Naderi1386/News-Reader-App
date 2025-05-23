aea35ca45e33e0b3923d1ae71d711bfb0fed122a
unknown <mh.naderi1386@gmail.com>
2025-05-23 15:48:55 +0330

    add a key for suspense in search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,7 +6,7 @@ import NewsList from "../_components/NewsList";
 
 interface SearchParamsType {
   q: string;
-  page?:string
+  page?: string;
 }
 
 interface PagePropsType {
@@ -23,13 +23,13 @@ export async function generateMetadata(props: PagePropsType) {
 const page = async (props: PagePropsType) => {
   const [query, page] = await Promise.all([
     (await props.searchParams).q || "news",
-    (await props.searchParams).page || "1"
+    (await props.searchParams).page || "1",
   ]);
   return (
     <div className="text-black px-4 md:px-18 py-18">
       <div className="space-y-10">
         <NewsBigTitle>News for {query}</NewsBigTitle>
-        <Suspense fallback={<Spinner />}>
+        <Suspense fallback={<Spinner />} key={`${query}&${page}`}>
           <NewsList q={query} page={page} />
         </Suspense>
       </div>
