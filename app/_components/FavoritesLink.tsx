53f4dfa7310efe8c1a4ffdd43bfc360cc82ef39e
unknown <mh.naderi1386@gmail.com>
2025-05-25 15:55:18 +0330

    working on favoriteslink component

--- app/_components/FavoritesLink.tsx
+++ app/_components/FavoritesLink.tsx
@@ -1,8 +1,10 @@
+import { auth } from "@/auth";
+import Link from "next/link";
 
-const FavoritesLink = () => {
-  return (
-    <div>FavoritesLink</div>
-  )
-}
+const FavoritesLink = async () => {
+  const session = await auth();
+  if (!session) return null;
+  return <Link href={"/favorites"}>Favorites ♥️</Link>;
+};
 
-export default FavoritesLink
+export default FavoritesLink;
