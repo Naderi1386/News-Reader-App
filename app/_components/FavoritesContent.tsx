20ff24524799245c15d2a528510a73e78b44f087
unknown <mh.naderi1386@gmail.com>
2025-05-27 13:34:38 +0330

    getting favorites items

--- app/_components/FavoritesContent.tsx
+++ app/_components/FavoritesContent.tsx
@@ -1,10 +1,8 @@
+import { getAllFavorites } from "../_lib/services";
 
-const FavoritesContent = () => {
-  return (
-    <div className="bg-white px-4 md:px-18 py-18">
-      FavoritesContent
-    </div>
-  );
-}
+const FavoritesContent = async () => {
+  const favorites = await getAllFavorites();
+  return <div className="bg-white px-4 md:px-18 py-18">FavoritesContent</div>;
+};
 
-export default FavoritesContent
+export default FavoritesContent;
