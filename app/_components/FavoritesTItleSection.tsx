e08ce012717495e0f97104cb0230bcba38ab88e5
unknown <mh.naderi1386@gmail.com>
2025-05-27 13:25:20 +0330

    change ui

--- app/_components/FavoritesTItleSection.tsx
+++ app/_components/FavoritesTItleSection.tsx
@@ -8,6 +8,10 @@ const FavoritesTitleSection = async () => {
     <div className="bg-custome-green text-white px-4 md:px-18 py-18">
       <div className="flex items-center gap-8">
         <FavoritesAvatar url={String(session.user?.image)} />
+        <h1 className="text-2xl font-semibold ">
+          Favorites news of{" "}
+          <span className="font-bold text-3xl capitalize">{session.user?.name}</span>
+        </h1>
       </div>
     </div>
   );
