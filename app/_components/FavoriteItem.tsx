0c0a15a5d6ee5966ec26b9e5ef009109f57587c8
unknown <mh.naderi1386@gmail.com>
2025-05-27 14:55:17 +0330

    add a link for favorites items

--- app/_components/FavoriteItem.tsx
+++ app/_components/FavoriteItem.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { FavoriteType } from "../_lib/services";
 import NewsPoster from "./NewsPoster";
 
@@ -7,9 +6,11 @@ interface FavoriteItemPropsType {
 }
 
 const FavoriteItem = ({ favorite }: FavoriteItemPropsType) => {
-  const { title, img, description } = favorite;
+  const { title, img, description, link } = favorite;
   const shortTitle = title.split(" ").slice(0, 10).join(" ");
-  const shortDescription = description ?description.split(" ").slice(0, 12).join(" ") : "Nothing"
+  const shortDescription = description
+    ? description.split(" ").slice(0, 12).join(" ")
+    : "Nothing";
   return (
     <li className="w-[22%]">
       <div className="card bg-base-100 image-full w-full shadow-sm">
@@ -20,7 +21,9 @@ const FavoriteItem = ({ favorite }: FavoriteItemPropsType) => {
           <h2 className="card-title text-sm">{shortTitle}...</h2>
           <p>{shortDescription}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <a target="_blank" href={link} className="btn bg-custome-green">
+              See More
+            </a>
           </div>
         </div>
       </div>
