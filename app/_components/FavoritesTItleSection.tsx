314aa93616de2f5ab738e885c23aff73ca474936
unknown <mh.naderi1386@gmail.com>
2025-05-27 13:13:35 +0330

    create a favoritesTitleSection component

---
+++ app/_components/FavoritesTItleSection.tsx
@@ -0,0 +1,8 @@
+
+const FavoritesTitleSection = () => {
+  return (
+    <div>FavoritesTItle</div>
+  )
+}
+
+export default FavoritesTitleSection;
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -3,6 +3,7 @@ const page = () => {
   return <div className="text-black px-4 md:px-18 py-18">
     <div>
       
+
     </div>
   </div>;
 }
