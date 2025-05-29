72ee21cbef1818914754f0880e075edda19e146a
unknown <mh.naderi1386@gmail.com>
2025-05-29 19:37:13 +0330

    working on ui in removefavbtn

--- app/_components/RemoveFavBtn.tsx
+++ app/_components/RemoveFavBtn.tsx
@@ -6,7 +6,7 @@ interface RemoveFavBtnPropsType {
 }
 const RemoveFavBtn = ({ id }: RemoveFavBtnPropsType) => {
   return (
-    <div className="flex justify-end py-2">
+    <div className="flex justify-end py-1">
       <button className="cursor-pointer">
         <MdOutlineDeleteOutline size={22} />
       </button>
