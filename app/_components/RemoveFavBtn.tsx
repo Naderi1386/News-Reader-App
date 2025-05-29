483025729d3d1c535a0a30fb796bf1f1926029c0
unknown <mh.naderi1386@gmail.com>
2025-05-29 19:35:42 +0330

    working on ui

--- app/_components/RemoveFavBtn.tsx
+++ app/_components/RemoveFavBtn.tsx
@@ -1,9 +1,17 @@
 "use client";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+
 interface RemoveFavBtnPropsType {
   id: number;
 }
 const RemoveFavBtn = ({ id }: RemoveFavBtnPropsType) => {
-  return <div className="flex justify-end py-2"></div>;
+  return (
+    <div className="flex justify-end py-2">
+      <button className="cursor-pointer">
+        <MdOutlineDeleteOutline size={22} />
+      </button>
+    </div>
+  );
 };
 
 export default RemoveFavBtn;
