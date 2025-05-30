a2f000102add43fa699dc9ed96cf762b5939b5ad
unknown <mh.naderi1386@gmail.com>
2025-05-30 15:48:51 +0330

    working on responsive

--- app/_components/FavoritesAvatar.tsx
+++ app/_components/FavoritesAvatar.tsx
@@ -7,7 +7,7 @@ interface FavoritesAvatarPropsType {
 const FavoritesAvatar = ({ url }: FavoritesAvatarPropsType) => {
   return (
     <div className="avatar">
-      <div className="mask mask-squircle size-16 relative">
+      <div className="mask mask-squircle size-12 sm:size-16 relative">
         <Image fill className="object-cover" alt="Avatar-User" src={url} />
       </div>
     </div>
--- app/_components/FavoritesTItleSection.tsx
+++ app/_components/FavoritesTItleSection.tsx
@@ -6,11 +6,11 @@ const FavoritesTitleSection = async () => {
   if (!session) return null;
   return (
     <div className="bg-custome-green text-white px-4 md:px-18 py-18">
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-4 sm:gap-8 flex-wrap">
         <FavoritesAvatar url={String(session.user?.image)} />
-        <h1 className="text-2xl font-semibold ">
+        <h1 className="text-lg sm:text-2xl font-semibold ">
           Favorites news of{" "}
-          <span className="font-bold text-3xl capitalize">
+          <span className="font-bold text-xl sm:text-3xl capitalize">
             {session.user?.name}
           </span>
         </h1>
