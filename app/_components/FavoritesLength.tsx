ef94208c94162f6dd333f9e73a19b7c3c27c7824
unknown <mh.naderi1386@gmail.com>
2025-05-30 15:50:55 +0330

    working on responsive p2

--- app/_components/FavoritesLength.tsx
+++ app/_components/FavoritesLength.tsx
@@ -6,11 +6,12 @@ const FavoritesLength = async () => {
   return (
     <div className="fixed left-0 right-0 bottom-0 bg-custome-green text-center py-5 text-white">
       {length > 1 ? (
-        <h3 className="text-xl">
-          There are <span className="font-bold">{length}</span> favorites news ♥️
+        <h3 className="text-lg sm:text-xl">
+          There are <span className="font-bold">{length}</span> favorites news
+          ♥️
         </h3>
       ) : (
-        <h3 className="text-xl">
+        <h3 className="text-lg sm:text-xl">
           There is <span className="font-bold">{length}</span> favorite ♥️
         </h3>
       )}
